2dcc3eb75583bf721c94599df76b9feaa1726002
Aecio Pires <aeciopires@gmail.com>
2016-12-26 17:06:55 -0300

    Adicionado o pacote "gosu" e ajustado o script de inicializacao do Grafana

--- ubuntu-grafana/run.sh
+++ ubuntu-grafana/run.sh
@@ -2,5 +2,17 @@
 
 set -e
 
+: "${GF_PATHS_DATA:=/var/lib/grafana}"
+: "${GF_PATHS_LOGS:=/var/log/grafana}"
+: "${GF_PATHS_PLUGINS:=/var/lib/grafana/plugins}"
+
+chown -R grafana:grafana "$GF_PATHS_DATA" "$GF_PATHS_LOGS"
+chown -R grafana:grafana /etc/grafana
+
 echo "=> Starting and running Grafana..."
-service grafana-server restart
+exec gosu grafana /usr/sbin/grafana-server  \
+  --homepath=/usr/share/grafana             \
+  --config=/etc/grafana/grafana.ini         \
+  cfg:default.paths.data="$GF_PATHS_DATA"   \
+  cfg:default.paths.logs="$GF_PATHS_LOGS"   \
+  cfg:default.paths.plugins="$GF_PATHS_PLUGINS"
